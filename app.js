45c1eb13b80df20968f03e5b399211e4adaa9074
kjh122333 <https://github.com/kjh122333>
2019-07-28 15:39:12 +0900

    dd

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
 const express = require('express')
 const app = express()
 
-app.set('veiw engine', 'ejs')
+
+//set the template engine ejs
+app.set('view engine', 'ejs')
+
+//middlewares
 app.use(express.static('public'))
 
+
+//routes
 app.get('/', (req, res) => {
-    res.send("HELLO")
+	res.render('index')
 })
 
-server = app.listen(30000);
+//Listen on port 30000
+server = app.listen(30000)
 
-const io = require('socket.io')(server)
 
-io.on('connect', (socket) =>{
 
-    console.log('New user connected')
-}) 
+//socket.io instantiation
+const io = require("socket.io")(server)
+
+
+//listen on every connection
+io.on('connection', (socket) => {
+	console.log('New user connected')
+
+	//default username
+	socket.username = "Anonymous"
+
+    //listen on change_username
+    socket.on('change_username', (data) => {
+        socket.username = data.username
+    })
+
+    //listen on new_message
+    socket.on('new_message', (data) => {
+        //broadcast the new message
+        io.sockets.emit('new_message', {message : data.message, username : socket.username});
+    })
+
+    //listen on typing
+    socket.on('typing', (data) => {
+    	socket.broadcast.emit('typing', {username : socket.username})
+    })
+})
